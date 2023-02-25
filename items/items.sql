4f3710f3d74453671a2b14830455f65b5195d54c
Emotion <111930499+Emotion06@users.noreply.github.com>
2023-02-25 03:37:46 +0100

    Add files via upload

---
+++ items/items.sql
@@ -0,0 +1,7 @@
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('chewingtobaccopack', 'Book', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('cigarpack', 'Book', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('cigpack', 'Book', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('fan', 'Book', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('parasol', 'Book', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('pipe', 'Book', 1, 1, 'item_standard', 1);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('pipepack', 'Book', 1, 1, 'item_standard', 1);
